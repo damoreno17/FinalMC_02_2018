745766dce985444f2b7faed16d1e57a21d7a9e5d
damoreno17 <42391091+damoreno17@users.noreply.github.com>
2018-11-28 19:04:55 -0500

    Update LEjercicio4.py

--- LEjercicio4.py
+++ LEjercicio4.py
@@ -12,18 +12,29 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from scipy.fftpack import fft, fftfreq, ifft
 
 n = 1280 # number of point in the whole interval
 f = 200.0 #  frequency in Hz
-dt = 1 / (f * 320 ) #320 samples per unit frequency
+dt = 1 / (f * 320 ) #El paso, 320 samples per unit frequency
 t = np.linspace( 0, (n-1)*dt, n)
 amp = np.cos(2 * np.pi * f * t) - 0.4 * np.sin(2 * np.pi * (2*f) * t ) + np.random.random(n)
 
 # SU FILTRO
+func (t, amp):
+  for amp in func
+    if amp > 1000 or amp < -1000
+      remove amp
+     else
+      amp2= amp
+      
+print (amp)
 
 # SU GRAFICA
-
+plt.plot (t,amp)
+plt.ṕlot(t,amp2)
+plt.show()
 
 # Puede usar los siguientes paquetes:
-#from scipy.fftpack import fft, fftfreq, ifft
+
 
