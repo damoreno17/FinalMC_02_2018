b2ee97b18551ce199d39b773a90fd33d685b4530
damoreno17 <42391091+damoreno17@users.noreply.github.com>
2018-11-28 19:20:14 -0500

    Update LEjercicio2.py

--- LEjercicio2.py
+++ LEjercicio2.py
@@ -12,3 +12,21 @@ import matplotlib.pylab as plt
 u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
 v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
 
+#para calcular la covarianza lo que debo hacer es:
+
+cov(u,v):
+  if len(u) !=  len(v)
+  return 
+
+   upro = np.mean(u)
+   vpro= np.mean(v)
+    
+   suma=0
+  
+  for i in range (0, len(u)):
+    suma += (u[i] -upro) * (v[i] - vpro)
+    
+  return suma/(len(u)-1)
+  
+    
+
